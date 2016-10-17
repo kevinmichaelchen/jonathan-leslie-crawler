2a5efee3918e1c96b7b217cb8432fee695d1fa42
Kevin Chen <kevinmichaelchen@gmail.com>
2016-10-16 20:18:47 -0400

    Longer timeouts; start scraping

--- src/main/scala/newsbank/Main.scala
+++ src/main/scala/newsbank/Main.scala
@@ -1,6 +1,7 @@
 package newsbank
 
 import java.util.Properties
+import scala.collection.JavaConverters._
 
 import newsbank.Links.formatUrl
 import org.jsoup.Jsoup
@@ -18,6 +19,8 @@ object Main {
   val RECURSE = false
   val BASE_URL = "http://infoweb.newsbank.com.ezproxy.soas.ac.uk"
 
+  var numArticlesScraped = 0
+
   def getCookie() = {
     val prop = new Properties()
     prop.load(getClass.getResourceAsStream("/secrets.properties"))
@@ -32,30 +35,47 @@ object Main {
   }
 
   def scrape(link: String, cookie: String): Unit = {
-    val doc = getLink(link, cookie)
+    val doc = get(link, cookie)
 
     val html = doc.html()
 
-//    println(html)
-    val articleLinks = doc.select("a.nb-doc-link")
-    println(articleLinks.size())
+    // Scrape articles
+    val articleLinks = doc.select("a.nb-doc-link").asScala
+    for( articleLink <- articleLinks ) {
+      val href = articleLink.attr("href")
+      if (numArticlesScraped != 0) {
+        return
+      }
+      scrapeArticle(BASE_URL + href, cookie)
+    }
 
+    // Go to the next page
     val pageLinks = doc.select("li.pager-item > a")
-    println(pageLinks.size())
-
     val nextLinkElement = pageLinks.first()
     if (nextLinkElement == null) {
       println("Done...")
     } else {
       val nextLink = nextLinkElement.attr("href")
       println(nextLink)
-
       if (RECURSE) scrape(BASE_URL + nextLink, cookie)
     }
   }
 
-  def getLink(link: String, cookie: String): Document =
-    Jsoup.connect(link)
+  def scrapeArticle(articleLink: String, cookie: String): Unit = {
+    val doc = get(articleLink, cookie)
+    val docHtml = doc.select("div.nb-doc")
+
+    val title = docHtml.select("div.title h2")
+
+    val source = docHtml.select("div.source")
+    println(doc)
+    numArticlesScraped += 1
+    println(articleLink)
+  }
+
+  def get(url: String, cookie: String): Document =
+    Jsoup.connect(url)
+      .timeout(6000)
       .cookie("ezproxy", cookie)
       // TODO fix timeout error
       .get
